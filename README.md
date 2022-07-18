ab9e92d06c0342ff5bc6762e428897295804326e
Shota Narisaka <shootacean@gmail.com>
2022-07-18 23:52:04 +0900

    feat(main): Hello, World

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # sandbox-zig
 
 [Getting Started ⚡ Zig Programming Language](https://ziglang.org/learn/getting-started/)
+
+```shell
+zig init-exe
+zig build run
+```
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    std.log.info("Hello, World", .{});
 }
 
 test "basic test" {
